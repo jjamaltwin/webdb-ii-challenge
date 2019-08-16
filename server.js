a720024ec1e1e0d5d70af2109829413eef9e9466
unknown <jjamal95@gmail.com>
2019-08-15 19:19:49 -0500

    finished endpoints

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const db = require('./data/car-model')
 const server = express();
 
 
-server.get('/accounts', (req, res) => {
+server.get('/cars', (req, res) => {
     db.READ().then(db => {
         res.status(200).json(db);
     }) .catch(err => {
-        res.status(500).json({success: false, err, message: "The acccount information could not be retrieved"});
+        res.status(500).json({success: false, err, message: "The car information could not be retrieved"});
+    });
+});
+
+server.post('/cars', (req, res) => {
+    db.CREATE().then(db => {
+        res.status(201).json(db);
+    }) .catch(err => {
+        res.status(500).json({success: false, err, message: "The car could not be added"});
     });
 });
 
